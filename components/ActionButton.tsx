75d57923d83ce98e850c879e711a3514e6a11e16
afzalfaizi <afzaalm993@gmail.com>
2024-03-21 12:27:44 +0500

    tech web

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Button } from './ui/button'
-import { AlignJustify } from 'lucide-react'
-import Link from 'next/link'
+import React from "react";
+import { Button } from "./ui/button";
+import { AlignJustify } from "lucide-react";
+import Link from "next/link";
 import {
   Sheet,
   SheetContent,
@@ -9,31 +9,35 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-
+} from "@/components/ui/sheet";
 
 export default function ActionButton() {
   return (
     <div>
-      <div className=' md:hidden'>
+      <div className=" md:hidden">
         <Sheet>
           <SheetTrigger>
-            <AlignJustify/>
+            <AlignJustify />
           </SheetTrigger>
           <SheetContent>
             <SheetHeader>
               <SheetDescription>
-                <div className=' flex flex-col space-y-4 w-full text-lg text-black mt-10'>
-                  <Link href={`/`}>
-                    Sign in
-                  </Link>
+                <div className=" flex flex-col space-y-4 w-full text-lg text-black mt-10">
+                  <Link href={`/`}>Sign in</Link>
+                  <Link href={`/`}>Features</Link>
+                  <Link href={`/`}>Pricing</Link>
+                  <Link href={`/`}>Contact</Link>
+                  <Link href={`/`}>About</Link>
                 </div>
               </SheetDescription>
             </SheetHeader>
           </SheetContent>
         </Sheet>
-
+      </div>
+      <div className=" hidden md:flex md:space-x-4">
+        <button className=" text-md">Sign in</button>
+        <button className=" text-md bg-blue-500 rounded p-2">Get Started</button>
       </div>
     </div>
-  )
+  );
 }
