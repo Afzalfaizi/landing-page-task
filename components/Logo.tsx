a13d810708b78a97073f9686ef7a3e7e23071637
afzalfaizi <afzaalm993@gmail.com>
2024-03-22 12:42:02 +0500

    Adding Comments

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import Image from "next/image";
+import React from "react"; // importing the React library
+import Image from "next/image"; // importing the Image component from Next.js
 
+// defining a functional component called Logo
 export default function Logo() {
   return (
     <div>
-      <Image src="/images/logo1.svg" alt="Logo" height={150} width={150} />
+      {" "}
+      {/* using the Image component from Next.js to display an image with alt text
+      and setting it as fluid so that it scales according to screen size that
+      acts as a container for the logo image that will contain our logo image */}
+      <Image
+        src="/images/logo1.svg" // specifying the source of the image
+        alt="Logo" // providing alternative text for the image
+        height={150} // setting the height of the image
+        width={150} // setting the width of the image
+      />
     </div>
   );
 }
