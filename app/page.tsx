d37efe3ce7b5c93f183710b6cf2a6440e26ea2a0
afzalfaizi <afzaalm993@gmail.com>
2024-03-22 19:09:31 +0500

    third section updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import HeroSection from "./hero-section/page";
 import Carousel from "@/components/ui/carousel";
 import SecondSection from "./second-section/page";
+import ThirdSection from "./third-section/page";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     <HeroSection/>
     <Carousel/>
     <SecondSection/>
+    <ThirdSection/>
    </div>
   );
 }
