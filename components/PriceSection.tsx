18a50c8e3f2f11a709421369792e5b16b461b7ce
afzalfaizi <afzaalm993@gmail.com>
2024-03-23 11:39:29 +0500

    faizi tech landing page

--- components/PriceSection.tsx
+++ components/PriceSection.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
+const features = [
+  {
+    name:"Basic",
+    price:"$40/month",
+    fees:"3.7% + 30¢ per transaction",
+    description:"Start selling online with a simple and easy to use platform. Create your first store in minutes.."
+  },
+  {
+    name:"LStandard",
+    price:"$80/month",
+    fees:"2.9% + 30¢ per transaction",
+    description:"Level up your business with a powerful eCommerce platform. Get access to all the features you need to grow."
+  },
+  {
+    name:"Premium",
+    price:"$200/month",
+    fees:"2.4% + 30¢ per transaction",
+    description:"For businesses that need more. Get access to all the features you need to grow."
+  }
+]
+
 export default function PriceSection() {
   return (
-    <div>PriceSection</div>
+    <div>
+      
+    </div>
   )
 }
